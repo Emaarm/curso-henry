8cb2ad365261164c2b925b43bf0635a3186d47e6
emaarm <emaarodri24@gmail.com>
2023-08-19 01:39:23 -0300

    2

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -5,6 +5,12 @@ function obtenerMayor(x, y) {
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
    // Tu código:
+
+   if (x !== y) {
+      return Math.max(x,y)
+   } else {
+      return (x || y);
+   }
 }
 
 function mayoriaDeEdad(edad) {
